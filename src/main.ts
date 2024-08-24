fa39e3041f30abe7222869d2ed6662c4d27b893d
samuelfreeman <samuelmanueljnr@gmail.com>
2024-08-24 23:29:07 +0000

    chore : setup swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //  use the document builder to create a new swagger document configuration
+  const config = new DocumentBuilder()
+    .setTitle("Autonomy Hall App Api")
+    .setDescription("API Documentation for Autonomy Hall App")
+    .setVersion("1.0")
+    .addTag("Autonomy Hall App")
+    .build()
+
+  //  create the Swagger document
+  const document = SwaggerModule.createDocument(app, config)
+
+  //  use the SwaggerModule to serve the Swagger document
+  SwaggerModule.setup("api", app, document)
+
+
   await app.listen(3000);
 }
 bootstrap();
