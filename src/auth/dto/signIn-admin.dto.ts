3f3e49c3adfc283205a419a4ce4bbee92f794778
samuelfreeman <samuelmanueljnr@gmail.com>
2024-09-08 03:57:05 +0000

    Api documentation :auth  schema

--- src/auth/dto/signIn-admin.dto.ts
+++ src/auth/dto/signIn-admin.dto.ts
@@ -3,16 +3,29 @@ import {
   IsOptional,
   IsEmail,
   IsStrongPassword,
-  IsInt,
+
   IsPhoneNumber,
 
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class signInAdminDto {
+  @ApiProperty({
+    example: 'john.doe@example.com',
+    required: true,
+  })
   @IsEmail()
   email: string;
+  @ApiProperty({
+    example: 'john.doe123!',
+    required: true,
+  })
   @IsStrongPassword()
   password: string;
+  @ApiProperty({
+    example: '+233245678901',
+    required: false,
+  })
   @IsOptional()
   @IsString()
   telephone?: string;
@@ -20,10 +33,22 @@ export class signInAdminDto {
 
 
 export class signInStudentDto {
+  @ApiProperty({
+    example: '52210408439',
+    required: true,
+  })
   @IsString()
   studentId: string;
+  @ApiProperty({
+    example: 'john.doe123!',
+    required: true,
+  })
   @IsStrongPassword()
   password: string;
+  @ApiProperty({
+    example: '+233245678901',
+    required: false,
+  })
   @IsOptional()
   @IsPhoneNumber('GH')
   telephone?: string;
