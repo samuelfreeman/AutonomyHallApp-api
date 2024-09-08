2ccf30643ad66ca46fcd329b9fe8d0c1f7507682
samuelfreeman <samuelmanueljnr@gmail.com>
2024-09-08 03:55:03 +0000

    Api documentation :allocation schema

--- src/allocation/dto/create-allocation.dto.ts
+++ src/allocation/dto/create-allocation.dto.ts
@@ -1,1 +1,21 @@
-export class CreateAllocationDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsNotEmpty } from "class-validator";
+
+
+export class CreateAllocationDto {
+
+    @ApiProperty({
+        example: "52210408439",
+        required: true
+    })
+    @IsNotEmpty()
+    @IsString()
+    studentId: string
+    @ApiProperty({
+        example: "suffisticatied room Id string",
+        required: true
+    })
+    @IsNotEmpty()
+    @IsString()
+    roomsId: string
+}
--- src/allocation/dto/update-allocation.dto.ts
+++ src/allocation/dto/update-allocation.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateAllocationDto } from './create-allocation.dto';
-
-export class UpdateAllocationDto extends PartialType(CreateAllocationDto) {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+export class UpdateAllocationDto extends PartialType(CreateAllocationDto) {
+    @ApiProperty({
+        example: "52210408439",
+        required: false
+    })
+    @IsNotEmpty()
+    @IsString()
+    roomsId?: string;
+    @ApiProperty({
+        example: "suffisticatied room Id string",
+        required: false
+    })
+    @IsNotEmpty()
+    @IsString()
+    studentId?: string;
+}
