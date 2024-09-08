2d290b6be184da8d8d02957c25f4586e69f5c626
samuelfreeman <samuelmanueljnr@gmail.com>
2024-09-08 03:58:48 +0000

    Api documentation :Hall  schema

--- src/hall/dto/create-hall.dto.ts
+++ src/hall/dto/create-hall.dto.ts
@@ -1,9 +1,17 @@
 import { IsString, IsArray } from "class-validator";
-
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateHallDto {
+    @ApiProperty({
+        example: "Hall of Science",
+        required: true,
 
+    })
     @IsString()
     name: string;
+    @ApiProperty({
+        example: ["East"],
+        required: true,
+    })
     @IsString()
     location: string;
 
--- src/hall/dto/update-hall.dto.ts
+++ src/hall/dto/update-hall.dto.ts
@@ -1,9 +1,22 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateHallDto } from './create-hall.dto';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UpdateHallDto extends PartialType(CreateHallDto) {
+    @ApiProperty({
+        example: '5f9875d6277706537c479690',
+        required: false,
+
+    })
     id?: string;
+    @ApiProperty({
+        example: 'Hall of Science',
+        required: false,
+    })
     name?: string;
+    @ApiProperty({
+        example: 'East',
+        required: false,
+    })
     location?: string;
-    
+
 }
